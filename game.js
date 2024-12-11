999d15127db5b9f76607ba54dc2aa9d15896e622
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-11 15:38:58 -0500

    Add function to play a round

--- game.js
+++ game.js
@@ -1,3 +1,34 @@
+let computerScore = 0;
+let playerScore = 0;
+
+playRound(1);
+
+function playRound(round) {
+  let playerChoice = getPlayerChoice();
+  let computerChoice = generateComputerChoice();
+
+  // Exit if player choice is null
+  if (!playerChoice) { return; }
+
+  console.log(`Round: ${round}\n` + `Player choice: ${playerChoice}\n` + `Computer choice: ${computerChoice}`);
+
+  if (playerChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    playerScore++;
+    console.log("You win this round!");
+  } else {
+    computerScore++;
+    console.log("Computer wins this round!");
+  }
+
+  console.log(`Your score so far is: ${playerScore}\n` + `Computer score so far is: ${computerScore}`);
+}
+
 function generateComputerChoice() {
   let computerChoice = ["rock", "paper", "scissors"];
   let randomNumber = Math.floor(Math.random() * 3); // Generate a random number from 1-3
